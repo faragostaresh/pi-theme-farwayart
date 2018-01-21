48ae734c87ddc5420a29c153a42e71ee72d20935
voltan <djvoltan@gmail.com>
2018-01-21 21:33:41 +0330

    work on theme

--- asset/js/front.js
+++ asset/js/front.js
@@ -1,18 +1,38 @@
 (function ($) {
-    //Fix navbar multiple level
-    var navbar = $('.pi-navbar-nav');
-    var hasBrand = navbar.parents('.navbar').find('.navbar-brand').length;
-    navbar.find('>li').each(function () {
-        var $this = $(this);
-        var caretStr = '<span class="pi-navbar-caret"></span>';
-        caretStr += '<span class="pi-navbar-caret pi-navbar-caret-outer"></span>';
-        if ($this.find('li').length) {
-            $this.append(caretStr);
+    $(document).ready(function () {
+        //Fix navbar multiple level
+        var navbar = $('.pi-navbar-nav');
+        var hasBrand = navbar.parents('.navbar').find('.navbar-brand').length;
+        navbar.find('>li').each(function () {
+            var $this = $(this);
+            var caretStr = '<span class="pi-navbar-caret"></span>';
+            caretStr += '<span class="pi-navbar-caret pi-navbar-caret-outer"></span>';
+            if ($this.find('li').length) {
+                $this.append(caretStr);
+            }
+        });
+        navbar.find('ul').addClass('dropdown-menu');
+
+        if (!hasBrand) {
+            navbar.css('marginLeft', '-15px');
         }
     });
-    navbar.find('ul').addClass('dropdown-menu');
 
-    if (!hasBrand) {
-        navbar.css('marginLeft', '-15px');
-    }
+    $(document).ready(function ($) {
+        $('.owl-carousel').owlCarousel({
+            margin: 3,
+            nav: true,
+            autoplay: true,
+            dots: false,
+            autoplayTimeout: 15000,
+            autoplayHoverPause: true,
+            navText: ['<i class="fa fa-angle-right"></i>', '<i class="fa fa-angle-left"></i>'],
+            responsive: {
+                0: {items: 1},
+                600: {items: 2},
+                1000: {items: 4}
+            }
+        })
+    });
 })(jQuery)
+
